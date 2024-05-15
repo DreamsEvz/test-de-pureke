a74efc4e69d7e021652971caa6045615c287e002
Evan EMA. MARCEL <emarcel@asylum.local>
2024-05-15 13:49:28 +0200

    add pie chart

--- app/components/questions.tsx
+++ app/components/questions.tsx
@@ -1,16 +1,20 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Questions = () => {
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [drugScore, setDrugScore] = useState(0);
   const [policeScore, setPoliceScore] = useState(0);
   const [hygieneScore, setHygieneScore] = useState(0);
-
   const [score, setScore] = useState(0);
+  const [percentage, setPercentage] = useState({
+    drug: 0,
+    police: 0,
+    hygiene: 0,
+  });
 
-  const questions: [string, number, string][] = [
+  const questions = [
     ["As tu déjà pris 4 drogues ou plus en une seule teuf ?", 30, "Drogue"],
     ["As tu déjà conduit sous LSD", 20, "Drogue"],
     ["As tu déjà fait des strats de fou pour vesqui les decks ?", 10, "Police"],
@@ -18,9 +22,25 @@ const Questions = () => {
     ["As tu déjà mangé un truc vraiment suspect en teuf ?", 10, "Hygiene"],
   ];
 
-  const nextQuestion = (anwser: string, categorie: string | null) => {
-    if (anwser === "yes") {
-      setScore((prevScore) => prevScore + questions[currentQuestion][1]);
+  const calculatePercentages = () => {
+    const totalScore = score;
+    if (totalScore === 0) return { drug: 0, police: 0, hygiene: 0 };
+    return {
+      drug: (drugScore / totalScore) * 100,
+      police: (policeScore / totalScore) * 100,
+      hygiene: (hygieneScore / totalScore) * 100,
+    };
+  };
+
+  useEffect(() => {
+    setPercentage(calculatePercentages());
+  }, [score, drugScore, policeScore, hygieneScore]);
+
+  const nextQuestion = (answer: string, category: string) => {
+    if (answer === "yes") {
+      setScore(
+        (prevScore) => prevScore + Number(questions[currentQuestion][1])
+      );
     }
 
     if (currentQuestion === questions.length - 1) {
@@ -29,21 +49,35 @@ const Questions = () => {
       setCurrentQuestion((prevQuestion) => prevQuestion + 1);
     }
 
-    switch (categorie) {
+    switch (category) {
       case "Drogue":
-        setDrugScore((prevScore) => prevScore + questions[currentQuestion][1]);
+        setDrugScore(
+          (prevScore) => prevScore + Number(questions[currentQuestion][1])
+        );
         break;
       case "Police":
         setPoliceScore(
-          (prevScore) => prevScore + questions[currentQuestion][1]
+          (prevScore) => prevScore + Number(questions[currentQuestion][1])
         );
         break;
       case "Hygiene":
         setHygieneScore(
-          (prevScore) => prevScore + questions[currentQuestion][1]
+          (prevScore) => prevScore + Number(questions[currentQuestion][1])
         );
         break;
     }
+  };
+
+  const getPieChartBackground = () => {
+    const drugEnd = percentage.drug;
+    const policeEnd = drugEnd + percentage.police;
+    const hygieneEnd = policeEnd + percentage.hygiene;
+    return `conic-gradient(
+      #DFFF00 0% ${drugEnd}%,
+      #FFBF00 ${drugEnd}% ${policeEnd}%,
+      #FF7F50 ${policeEnd}% ${hygieneEnd}%,
+      #fff ${hygieneEnd}% 100%
+    )`;
   };
 
   return (
@@ -60,36 +94,44 @@ const Questions = () => {
         {currentQuestion !== -1 ? (
           <>
             <button
-              onClick={() => nextQuestion("yes", questions[currentQuestion][2])}
+              onClick={() =>
+                nextQuestion("yes", String(questions[currentQuestion][2]))
+              }
               className="bg-green-600 p-4 rounded-sm"
             >
               Oui
             </button>
             <button
-              onClick={() => nextQuestion("no", null)}
+              onClick={() => nextQuestion("no", "")}
               className="bg-red-500 p-4 rounded-sm"
             >
               Non
             </button>
           </>
         ) : (
-          <div className="flex flex-col gap-4 mt-24">
-            <p>
-              Ton score drogue est de{" "}
-              <span className="text-red-500">{drugScore}</span>
-            </p>
-            <p>
-              Ton score police est de{" "}
-              <span className="text-red-500">{policeScore}</span>
-            </p>
-            <p>
-              Ton score hygiène est de{" "}
-              <span className="text-red-500">{hygieneScore}</span>
-            </p>
-            <p className="text-xl">
-              Ton score total est de{" "}
-              <span className="text-red-500">{score}</span>
-            </p>
+          <div className="flex flex-col mt-48">
+            <div
+              className="rounded-full h-40 w-40 relative perspective-3d"
+              style={{
+                background: getPieChartBackground(),
+              }}
+            ></div>
+            <div className="flex flex-col justify-between mt-4">
+              <p>
+                {" "}
+                <span className="text-[#DFFF00]">Drogue</span>:{" "}
+                {percentage.drug.toFixed(2)}%
+              </p>
+              <p>
+                {" "}
+                <span className="text-[#FFBF00]">Police</span>:{" "}
+                {percentage.police.toFixed(2)}%
+              </p>
+              <p>
+                <span className="text-[#FF7F50]">Hygiène</span>:{" "}
+                {percentage.hygiene.toFixed(2)}%
+              </p>
+            </div>
           </div>
         )}
       </div>
